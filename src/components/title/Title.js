2f481584bbf512797ad4d2bd4171ee75213bf469
WiktorPospiech <wiktor.pospiech@gmail.com>
2023-10-28 09:38:37 +0200

    title component added

---
+++ src/components/title/Title.js
@@ -0,0 +1,9 @@
+import styles from 'components/title/title.module.css';
+
+export default function Title() {
+  return (
+    <div>
+      <header className={styles.header}>Please leave feadback</header>
+    </div>
+  );
+}
